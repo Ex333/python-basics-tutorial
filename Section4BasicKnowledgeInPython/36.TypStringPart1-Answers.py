2f7b053d1990d2a8ab5a9e3e3a4ee8f28d416f27
Ex333 <haldas.mateusz@gmail.com>
2026-01-14 16:01:32 +0100

    Add solutions for String type Part 1 lab

---
+++ Section4BasicKnowledgeInPython/36.TypStringPart1-Answers.py
@@ -0,0 +1,25 @@
+#1
+quote = "A good programmer is someone who always looks both ways before crossing a one-way street."
+#2
+print(quote.upper())
+#3
+print(quote.lower())
+#4
+print(quote.endswith("street"))
+#5
+print(quote.isupper())
+#6
+print(quote.upper().isupper())
+#7
+print(quote.find("one", 0))
+#8
+print(quote.replace("one", "1"))
+#9
+print(quote.replace("one", "1").replace("both", "2"))
+#10
+print(quote.split(" "))
+#11
+print(quote.isdigit())     # Check if the string is a number
+print(quote.isdecimal())   # Check if the string is a decimal number
+print(quote.isalpha())     # Check if the string contains only letters
+print(quote.isalnum())     # Check if the string contains letters and digits
